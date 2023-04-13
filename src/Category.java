91626a5a336cdbda158ad9eb3420d455c0d8cf60
Amirhossein <Amirmemool12@gmail.com>
2023-04-13 09:22:35 -0400

    add category call

---
+++ src/Category.java
@@ -0,0 +1,17 @@
+public class Category {
+    private String id;
+    private String name;
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
