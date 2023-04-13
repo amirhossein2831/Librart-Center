0488ecf2f54657f0d8adfe2c50c852533c087c99
Amirhossein <Amirmemool12@gmail.com>
2023-04-13 11:50:46 -0400

    delete hashSet of book and thesis from category

--- src/Category.java
+++ src/Category.java
@@ -3,14 +3,9 @@ import java.util.HashSet;
 public class Category {
     private String id;
     private String name;
-
-    private HashSet<String> bookId;
-    private HashSet<String> thesisId;
-
     public Category(String id, String name) {
         this.id = id;
         this.name = name;
-        bookId = new HashSet<>();
     }
 
     public String getId() {
@@ -21,19 +16,4 @@ public class Category {
         return name;
     }
 
-    public HashSet<String> getBookId() {
-        return bookId;
-    }
-
-    public HashSet<String> getThesisId() {
-        return thesisId;
-    }
-
-    public void addBookId(String bookId) {
-        this.bookId.add(bookId);
-    }
-
-    public void addThesisId(String thesisId) {
-        this.thesisId.add(thesisId);
-    }
 }
