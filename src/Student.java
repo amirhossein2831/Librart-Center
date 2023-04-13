1f0d1d345d9c16144ecca8f3a4a7004534f51c8c
Amirhossein <Amirmemool12@gmail.com>
2023-04-13 13:06:50 -0400

    add Student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,47 @@
 public class Student {
+    private String id;
+    private String pass;
+    private String firstName;
+    private String lastName;
+    private String nationalId;
+    private String year;
+    private String address;
+
+    public Student(String id, String pass, String firstName, String lastName, String nationalId, String year, String address) {
+        this.id = id;
+        this.pass = pass;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalId = nationalId;
+        this.year = year;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
