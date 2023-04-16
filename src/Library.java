cac73e3d88f01a1bc7e89bf2ca8bae08d51dee12
Amirhossein <Amirmemool12@gmail.com>
2023-04-16 09:46:29 -0400

    add count borrow and borrow to library class

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Library {
     private String id;
@@ -9,6 +11,7 @@ public class Library {
     private String address;
     private HashMap<String, Book> books;
     private HashMap<String, Thesis> theses;
+    private HashMap<String, ArrayList<Borrow>> borrows;
     public Library(String id, String name, String year, int numSeat, String address) {
         this.id = id;
         this.name = name;
@@ -17,6 +20,7 @@ public class Library {
         this.address = address;
         books = new HashMap<>();
         theses = new HashMap<>();
+        borrows = new HashMap<>();
     }
 
     public String getId() {
@@ -61,4 +65,44 @@ public class Library {
     public void removeThesis(String id) {
         theses.remove(id);
     }
+
+    public HashSet<String> getBookIds() {
+        return new HashSet<>(books.keySet());
+    }
+
+    public HashSet<String> getThesisIds() {
+        return new HashSet<>(theses.keySet());
+    }
+
+    public boolean borrow(Borrow borrow) {
+        ArrayList<Borrow> borrows1 = new ArrayList<>();
+        if (borrow.isStudent()) {
+            if (countBorrows(borrow.getUserId()) < 3) {
+                borrows1.add(borrow);
+                borrows.put(borrow.getStuffId(),borrows1);
+                return true;
+            }
+            return false;
+        }
+        if (countBorrows(borrow.getUserId()) < 5) {
+            borrows1.add(borrow);
+            borrows.put(borrow.getStuffId(),borrows1);
+            return true;
+        }
+        return false;
+    }
+
+    public int countBorrows(String userId) {
+        int count = 0;
+        for (ArrayList<Borrow> stuffBorrows : new ArrayList<>(borrows.values())) {
+            for (Borrow borrow : stuffBorrows) {
+                if (borrow.getUserId().equals(userId)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
 }
